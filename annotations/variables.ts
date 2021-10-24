d6a7973e47d70d5e5560029f682ad218a82ea6c1
Jon <jbrundagejr@gmail.com>
2021-10-24 14:10:52 -0400

    variables done

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -9,3 +9,44 @@ let now: Date = new Date()
 
 // Array
 let colors: string[] = ['red', 'green', 'blue']
+let myNumbers: number[] = [1, 2, 3]
+let truths: boolean[] = [true, true, false]
+
+// Classes
+class Car {
+
+}
+
+let car: Car = new Car()
+
+// Object Literal
+let point: {x: number; y: number} = {
+  x: 10,
+  y:20
+}
+
+// Function - need to define what arguments come in and what the function returns. Void if it doesn't return anything
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i)
+}
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}'
+const coordinates: {x: number; y: number} = JSON.parse(json)
+// 2) We want to declare a variable on one line but initalize it later
+let words = ['red', 'green', 'blue']
+let foundWord: boolean
+for (let i = 0; i < words.length; i++){
+  if(words[i] === 'green'){
+    foundWord = true
+  }
+}
+// 3) We want a variable to have a type that can't be inferred
+let numbers = [-10, -1, 12]
+let numberAboveZero: boolean | number = false
+for (let i = 0; i < numbers.length; i++){
+  if(numbers[i] > 0){
+    numberAboveZero = numbers[i]
+  }
+}
